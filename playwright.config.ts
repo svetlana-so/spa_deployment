bbe4942dcba20bc1c4d3018d261fe01126d1486e
Svetlana Soboleva <sobolevasvetlanaalex@gmail.com>
2023-11-27 20:54:26 +0100

    ci: autotests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,11 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 import { devices } from '@playwright/test'
+const formBaseURL = (baseURL: string) => {
+  const repo = process.env.GITHUB_REPOSITORY
+  const basePath = repo ? `/${repo.split('/')[1]}/` : ''
 
+  return `${baseURL}${basePath}`
+}
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -34,7 +39,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: formBaseURL('http://localhost:5173'),
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
